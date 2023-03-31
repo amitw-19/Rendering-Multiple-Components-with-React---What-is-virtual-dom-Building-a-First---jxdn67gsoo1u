b329229fe04ec4750e7173d5b479376b73892995
amitw-19 <tarunwarathe@gmail.com>
2023-03-31 20:21:54 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1 data-ns-test="project-name">YouTube Clone</h1>
+      <p data-ns-test="project-description">
+        It will be a youtube clone using tech stack - HTML, CSS, JavaScript,
+        React, Node.js.
+      </p>
+    </div>
+  );
+};
 
 export default App;
